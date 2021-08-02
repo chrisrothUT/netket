4a0d407b352cc099a529b57c594f2d36a3d1a63a
chrisrothUT <61942272+chrisrothUT@users.noreply.github.com>
2021-08-02 11:32:31 -0500

    Update mpi_stats.py

--- netket/stats/mpi_stats.py
+++ netket/stats/mpi_stats.py
@@ -21,15 +21,12 @@ def subtract_mean(x, axis=None):
     """
     Subtracts the mean of the input array over all but the last dimension
     and over all MPI processes from each entry.
-
     Args:
         x: Input array
         axis: Axis or axes along which the means are computed. The default (None) is to
               compute the mean of the flattened array.
-
     Returns:
         The resulting array.
-
     """
     # here we keep the dims, since automatic broadcasting of a scalar (shape () ) to an array produces errors
     # when used inside of a function which is transposed with jax.linear_transpose
@@ -40,20 +37,16 @@ def subtract_mean(x, axis=None):
 def mean(a, axis=None, keepdims: bool = False):
     """
     Compute the arithmetic mean along the specified axis and over MPI processes.
-
     Returns the average of the array elements. The average is taken over the flattened array by default,
     otherwise over the specified axis. float64 intermediate and return values are used for integer inputs.
-
     Args:
         a: The input array
         axis: Axis or axes along which the means are computed. The default (None) is to
               compute the mean of the flattened array.
         keepdims: If True the output array will have the same number of dimensions as the input,
               with the reduced axes having length 1. (default=False)
-
     Returns:
         The array with reduced dimensions defined by axis.
-
     """
     out = a.mean(axis=axis, keepdims=keepdims)
 
@@ -64,7 +57,6 @@ def mean(a, axis=None, keepdims: bool = False):
 def sum(a, axis=None, keepdims: bool = False):
     """
     Compute the sum along the specified axis and over MPI processes.
-
     Args:
         a: The input array
         axis: Axis or axes along which the mean is computed. The default (None) is to
@@ -72,10 +64,8 @@ def sum(a, axis=None, keepdims: bool = False):
         out: An optional pre-allocated array to fill with the result.
         keepdims: If True the output array will have the same number of dimensions as the input,
               with the reduced axes having length 1. (default=False)
-
     Returns:
         The array with reduced dimensions defined by axis. If out is not none, returns out.
-
     """
     # if it's a numpy-like array...
     if hasattr(a, "shape"):
@@ -89,11 +79,14 @@ def sum(a, axis=None, keepdims: bool = False):
     return out
 
 
-def var(a, axis=None, ddof: int = 0):
+# needed othewise it is shadowed...
+_mean = mean
+
+
+def var(a, axis=None, ddof: int = 0, mean=None):
     """
     Compute the variance mean along the specified axis and over MPI processes.
     Assumes same shape on all MPI processes.
-
     Args:
         a: The input array
         axis: Axis or axes along which the variance is computed. The default (None) is to
@@ -101,17 +94,16 @@ def var(a, axis=None, ddof: int = 0):
         out: An optional pre-allocated array to fill with the result.
         ddof: “Delta Degrees of Freedom”: the divisor used in the calculation is N - ddof,
               where N represents the number of elements. By default ddof is zero.
-
     Returns:
         The array with reduced dimensions defined by axis. If out is not none, returns out.
-
     """
-    m = mean(a, axis=axis)
+    if mean is None:
+        mean = _mean(a, axis=axis)
 
     if axis is None:
-        ssq = jnp.abs(a - m) ** 2.0
+        ssq = jnp.abs(a - mean) ** 2.0
     else:
-        ssq = jnp.abs(a - jnp.expand_dims(m, axis)) ** 2.0
+        ssq = jnp.abs(a - jnp.expand_dims(mean, axis)) ** 2.0
 
     out = sum(ssq, axis=axis)
 
@@ -124,13 +116,10 @@ def var(a, axis=None, ddof: int = 0):
 def total_size(a, axis=None):
     """
     Compute the total number of elements stored in the input array among all MPI processes.
-
     This function essentially returns MPI_sum_among_processes(a.size).
-
     Args:
         a: The input array.
         axis: If specified, only considers the total size of that axis.
-
     Returns:
         a.size or a.shape[axis], reduced among all MPI processes.
     """
